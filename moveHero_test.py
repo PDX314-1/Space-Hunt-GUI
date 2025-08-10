606d3f4aa63384d79150aca2989cfd2ad1f2a2e1
PDX314-1 <grabpage@yahoo.com>
2025-08-10 11:30:43 -0700

    Changed Function Name

--- moveHero_test.py
+++ moveHero_test.py
@@ -1,9 +1,14 @@
 import pytest
 import shared_items
-def move_hero():
+def move_hero(angle_move, distance_move, game_state):
+    x_current = game_state['x']
+    y_current = game_state['y']
+    print("X: ",x_current,"     Y: ",y_current)
     return ("Hero Moved")
 
 
 
 def test_1():
-    assert move_hero() == "Hero Stopped"
+    assert move_hero(0,1,{'x':0, 'y':0, 'energy':1000, 'supplies': 100, 'money':1000, 'message':''}) == "Hero Moved"
+
+#move_hero(0,1,{'x':10, 'y':20, 'energy':1000, 'supplies': 100, 'money':1000, 'message':''})
--- shared_items.py
+++ shared_items.py
@@ -4,3 +4,5 @@ current_x = 0
 current_y = 0
 energy = 1000
 supplies = 100
+
+game_state = {'x':0, 'y':0, 'energy':1000, 'supplies': 100, 'money':1000, 'message':''}
